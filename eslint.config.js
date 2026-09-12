e23c987c025e90883880e57d4f145243af8b9733
Useplusprojectlead <useplusproject3@gmail.com>
2026-09-12 19:33:17 +0200

    Ignore deploy checkout in lint

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
 export default [
-  { ignores: ['dist', 'vite.config.js', 'vite.config.d.ts'] },
+  { ignores: ['dist/**', '.deploy-pages/**', 'vite.config.js', 'vite.config.d.ts'] },
   ...tseslint.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
